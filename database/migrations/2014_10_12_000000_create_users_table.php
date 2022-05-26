b05045e77d24eeba9dce7d30ebd8e24b370921a1
Dhiraj <dhiraj2learn@gmail.com>
2022-05-26 23:26:21 +0530

    Second Commit: Authorisation installed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name');            
             $table->string('email')->unique();
+            $table->string('phone');
+            $table->tinyInteger('role')->default('0');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
